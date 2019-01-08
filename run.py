ff39002320d90c8651df8b531648e5f4546ef72f
Irina Nita <irinanita.im@gmail.com>
2019-01-08 17:49:08 +0000

    Added html templates & basic structure

--- run.py
+++ run.py
@@ -1,10 +1,23 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, url_for, session, request
 
 app = Flask(__name__)
+app.secret_key = os.getenv("SECRET", "not a secret")
 
 @app.route('/')
 def index():
-    return "<h1>Hello Quiz!</h1>"
+    return render_template('index.html')
+    
+@app.route('/game', methods = ['POST'])
+def game():
+    session['username']= request.form['username']
+    return render_template('game.html', username = session['username'])
+    
+@app.route('/leaderboard')
+def leaderboard():
+    return render_template('leaderboard.html')
 
-app.run(host = os.getenv('IP'),port = int(os.getenv('PORT')), debug = True)    
+if __name__ == '__main__':
+    app.run(host = os.getenv('IP'), 
+            port = int(os.getenv('PORT')),
+            debug = True)  
